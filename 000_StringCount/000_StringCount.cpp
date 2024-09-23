9cee383527f44e5cb54c1e761edb76efa10f7edd
민정 강 <rkddkwl3414@naver.com>
2024-09-23 14:29:21 +0900

    강민정_StringCount과제

---
+++ 000_StringCount/000_StringCount.cpp
@@ -0,0 +1,24 @@
+﻿// 000_StringCount.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include <iostream>
+
+int StringCount(const char* const _Str)
+{
+    int Count = 0;
+
+    while (_Str[Count] != 0)
+    {
+        Count += 1;
+    }
+
+    return Count;
+}
+
+int main()
+{
+    // [B][B][B][B][0]
+    int Count = strlen("BBBB");
+    int MyCount = StringCount("BBBB");
+
+    int a = 0;
+}
