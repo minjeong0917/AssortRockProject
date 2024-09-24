7f05795aa5ba0e4753003a0120841a4701c63856
민정 강 <rkddkwl3414@naver.com>
2024-09-24 14:19:42 +0900

    강민정 : TextRPG000 과제

---
+++ 003_PlusString/003_PlusString.cpp
@@ -0,0 +1,35 @@
+﻿// 003_PlusString.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+
+const int IntMaxCount = 10;
+
+void PlusString(char* _Buffer, int _BufferSize, const char* const _Value0, const char* const _Value1)
+{
+
+	int ChCount = 0;
+
+	for (int i = 0; i < 2; i++)
+	{
+		_Buffer[ChCount] = _Value0[ChCount];
+		putchar(_Value0[ChCount]);
+		ChCount += 1;
+	}
+
+
+	return;
+
+}
+
+int main()
+{
+    char Arr[100] = {};
+
+    PlusString(Arr, 100, "AAAA", "BBBB");
+
+	int a = 1;
+
+}
+
---
+++ 004_TextRPG000/004_TextRPG000.cpp
@@ -0,0 +1,91 @@
+﻿// 004_TextRPG000.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+const int LINECOUNT = 50;
+const int NAMELEN = 10;
+
+int PlayerAtt = 0;
+int PlayerHp = 0;
+char PlayerName[NAMELEN] = "NONE";
+
+int MonsterAtt = 10;
+int MonsterHp = 100;
+char MonsterName[NAMELEN] = "NONE";
+
+void StrCopy(char* _Arr, int _BufferSize, const char* const _Name)
+{
+    for (int i = 0; i < _BufferSize; i += 1)
+    {
+        _Arr[i] = 0;
+    }
+
+
+    int Size = strlen(_Name);
+    _Arr[0] = _Name[0];
+
+    for (int i = 0; i < Size; i++)
+    {
+        _Arr[i] = _Name[i];
+    }
+}
+void CreatePlayer(const char* const _Ptr, int _Att, int _Hp)
+{
+    StrCopy(PlayerName, NAMELEN, _Ptr);
+    PlayerAtt = _Att;
+    PlayerHp = _Hp;
+}
+
+void CreateMonster(const char* const _Ptr, int _Att, int _Hp)
+{
+    StrCopy(MonsterName, NAMELEN, _Ptr);
+    MonsterAtt = _Att;
+    MonsterHp = _Hp;
+}
+
+void Line(int _size)
+{
+
+    for (int i = 0; i < LINECOUNT - _size; i += 1)
+    {
+        printf_s("-");
+    }
+    printf_s("\n");
+
+}
+
+void StatusRender(const char* _Name, int _Att, int _HP)
+{
+
+    printf_s("%s Status ", _Name);
+
+    int NameSize = strlen(_Name) + strlen(" Status ");
+
+    Line(NameSize);
+
+    printf_s("공격력 : %d\n", _Att);
+    printf_s("체력 : %d\n", _HP);
+
+    Line(0);
+
+}
+
+void PlayerStatusRender()
+{
+    StatusRender(PlayerName, PlayerAtt, PlayerHp);
+}
+
+void MonsterStatusRender()
+{
+    StatusRender(MonsterName, MonsterAtt, MonsterHp);
+}
+int main()
+{
+    CreatePlayer("Playertest", 10, 100);
+    CreateMonster("Orc", 10, 50);
+
+    PlayerStatusRender();
+    MonsterStatusRender();
+
+}
